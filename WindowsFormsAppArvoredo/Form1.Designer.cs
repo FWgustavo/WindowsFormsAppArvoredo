a976893abd083ac92cb999a73a9026ab100b57fc
231024 <231024@CPD03-M18>
2025-03-27 15:54:26 -0300

    Adicionar arquivos de projeto.

---
+++ WindowsFormsAppArvoredo/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace WindowsFormsAppArvoredo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Btn_Login = new System.Windows.Forms.Button();
+            this.Btn_Config = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Gagalin", 45F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(27)))), ((int)(((byte)(1)))));
+            this.label1.Location = new System.Drawing.Point(347, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(307, 72);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ARVOREDO ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Btn_Login
+            // 
+            this.Btn_Login.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(212)))), ((int)(((byte)(172)))));
+            this.Btn_Login.FlatAppearance.BorderSize = 0;
+            this.Btn_Login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_Login.Font = new System.Drawing.Font("Gagalin", 45F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Login.Location = new System.Drawing.Point(242, 317);
+            this.Btn_Login.Name = "Btn_Login";
+            this.Btn_Login.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Btn_Login.Size = new System.Drawing.Size(489, 86);
+            this.Btn_Login.TabIndex = 1;
+            this.Btn_Login.Text = "Login";
+            this.Btn_Login.UseCompatibleTextRendering = true;
+            this.Btn_Login.UseVisualStyleBackColor = false;
+            this.Btn_Login.Click += new System.EventHandler(this.Btn_Login_Click);
+            // 
+            // Btn_Config
+            // 
+            this.Btn_Config.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(212)))), ((int)(((byte)(172)))));
+            this.Btn_Config.FlatAppearance.BorderSize = 0;
+            this.Btn_Config.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_Config.Font = new System.Drawing.Font("Gagalin", 45F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Config.Location = new System.Drawing.Point(242, 427);
+            this.Btn_Config.Name = "Btn_Config";
+            this.Btn_Config.Size = new System.Drawing.Size(489, 85);
+            this.Btn_Config.TabIndex = 2;
+            this.Btn_Config.Text = "Configuração";
+            this.Btn_Config.UseCompatibleTextRendering = true;
+            this.Btn_Config.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::WindowsFormsAppArvoredo.Properties.Resources.logo1;
+            this.pictureBox1.Location = new System.Drawing.Point(412, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(154, 172);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1011, 617);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Btn_Config);
+            this.Controls.Add(this.Btn_Login);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Btn_Login;
+        private System.Windows.Forms.Button Btn_Config;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsAppArvoredo/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppArvoredo
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+
+        private static extern IntPtr CreateRoundRectRgn
+            (
+               int nLeft,
+               int nTop,
+               int nRight,
+               int nBottom,
+               int nWidthEllipse,
+               int nHeightEllipse
+            );
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SetBackColorDegrade(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+
+            Rectangle gradient_rect = new Rectangle(0, 0, Width, Height);
+
+            Brush br = new LinearGradientBrush(gradient_rect, Color.FromArgb(250, 230, 194), Color.FromArgb(198, 143, 86), 90f);
+
+            graphics.FillRectangle(br, gradient_rect);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            SetBackColorDegrade(sender, e);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Btn_Login.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Btn_Login.Width, Btn_Login.Height, 100, 100));
+            Btn_Config.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Btn_Config.Width, Btn_Config.Height, 100, 100));
+        }
+
+        private void Btn_Login_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
